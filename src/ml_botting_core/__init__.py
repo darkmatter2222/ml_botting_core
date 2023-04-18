4ef0575f5d67372726f4a44b5e5ee87edf83f629
ryan susman <ryansusman@gmail.com>
2023-04-18 19:20:14 -0400

    building

--- src/ml_botting_core/__init__.py
+++ src/ml_botting_core/__init__.py
@@ -1,8 +1,1 @@
-import sys
-
-try:
-    sys.modules['ml_botting_core'] = sys.modules['src.ml_botting_core']
-except:
-    pass
-
-from ml_botting_core.universal_predictor.universal_predictor import universal_predictor
+from .base import universal_predictor
